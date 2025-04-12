1fb57af7692ee2d17b4042b0157ff782d259bb33
Kavya <venkatakavya734@gmail.com>
2025-04-12 14:04:51 +0530

    Add Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Hero from "./sections/Hero";
+
 const App = () => {
   return (
-    <main>
-      <div>
-        <h1>welcome to my 3D portfolio!</h1>
-      </div>
-    </main>
+    <>
+      <Hero />
+    </>
   );
 };
 
