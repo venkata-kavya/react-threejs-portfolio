d20a20556b31f9348048b730c88a698d003ee3f1
Kavya <venkatakavya734@gmail.com>
2025-04-15 20:46:35 +0530

    Add feature cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import LogoSection from "./components/logoSection";
+import LogoSection from "./sections/LogoSection";
 import NavBar from "./components/NavBar";
+import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 
@@ -9,7 +10,8 @@ const App = () => {
       <NavBar />
       <Hero />
       <ShowcaseSection />
-      <LogoSection />
+      {/* <LogoSection /> */}
+      <FeatureCards />
     </>
   );
 };
