99fb103d30ad2d833c7eff21ab3e884b78a2a665
Kavya <venkatakavya734@gmail.com>
2025-04-15 19:58:05 +0530

    Add logo section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import LogoSection from "./components/logoSection";
 import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
@@ -8,6 +9,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
     </>
   );
 };
---
+++ src/components/LogoSection.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { logoIconsList } from "../constants";
+
+const LogoIcon = ({ icon }) => {
+  return (
+    <div className="flex-none flex-center marquee-item">
+      <img src={icon.imgPath} alt={icon.name} />
+    </div>
+  );
+};
+
+const LogoSection = () => {
+  return (
+    <div className="md:my-20 my-10 relative">
+      <div className="gradient-edge" />
+      <div className="gradient-edge" />
+
+      <div className="marquee h-52">
+        <div className="marquee-box md:gap-12 gap-5">
+          {logoIconsList.map((icon) => (
+            <LogoIcon key={icon.name} icon={icon} />
+          ))}
+        </div>
+
+        <div className="marquee-box md:gap-12 gap-5">
+          {logoIconsList.map((icon) => (
+            <LogoIcon key={icon.name} icon={icon} />
+          ))}
+        </div>
+        {/* creating duplicate streams of logos for seamless scroll */}
+      </div>
+    </div>
+  );
+};
+
+export default LogoSection;
