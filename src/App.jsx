dea2ae022694d8df55fb33dfb7df3b3f4322fa4e
Kavya <venkatakavya734@gmail.com>
2025-04-18 23:21:22 +0530

    Add contact Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
+import Contact from "./sections/Contact";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <FeatureCards />
       <ExperienceSection />
       <TechStack />
+      <Contact />
     </>
   );
 };
